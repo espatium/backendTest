59442a5d4b9773906ce819d7cd1fa4456b11a484
bk choi <espatium.main@gmail.com>
2022-07-17 19:53:04 +0900

    git TEST commit

---
+++ api.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+const server = app.linsten(3001, () => {
+    console.log('Start Server : 54.180.1.96:3001');
+
+});
+
+app.get('/api/users/:type', async(req, res) => {
+    res.send('connect.');
+});
+
+
+
+
+
+
