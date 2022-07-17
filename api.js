2e68e214b3ca9f1e7aa3a63b3d779bb566046af4
espatium <99029937+espatium@users.noreply.github.com>
2022-07-17 22:36:16 +0900

    Update api.js

--- api.js
+++ api.js
@@ -2,19 +2,52 @@ const express = require('express');
 
 const app = express();
 
+var mysql = require('mysql');
+var conn = mysql.createConnection({
+    host : '54.180.1.96',
+    user : 'ubuntu',
+    password : 'choi#',
+    database : 'espa'
+});
+
+conn.connect();
+
 const server = app.listen(3001, () => {
     console.log('Start Server : 54.180.1.96:3001');
 
 });
 
+// 콜론이 있으면 어떤값이든 들어올수 있다는 의미임
+// app.get('/api/users/:type', async(req, res) => {
+//     res.send('connect.');
+// });
+//
+
 app.get('/api/users/:type', async(req, res) => {
+
     let {type} = req.params;
 
     console.log(type);
+
+
+    conn.query('select * from users', function(err, rows, fields) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('rows', rows);
+            console.log('fields', fields);
+        }
+        
+    });
+
+
     res.send('ok');
+
+
 });
 
 
+conn.end();
 
 
 
