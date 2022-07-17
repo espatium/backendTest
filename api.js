343f02569e66ec8e906ef9fbf5d0ee2016278343
espatium <99029937+espatium@users.noreply.github.com>
2022-07-17 20:04:44 +0900

    Update api.js

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const server = app.linsten(3001, () => {
+const server = app.listen(3001, () => {
     console.log('Start Server : 54.180.1.96:3001');
 
 });
