19fa3ac59e0f666ba7c86cb3ea12bf09a7afbf94
espatium <99029937+espatium@users.noreply.github.com>
2022-07-23 13:23:46 +0900

    Update api.js

--- api.js
+++ api.js
@@ -78,7 +78,7 @@ app.get('/api/users/cid/:type', async(req, res) => {
             console.log(err1);
         } else {
             console.log(rows1);
-            let data_id = rows1[0].id;
+            let data_id = rows1;
             console.log(data_id);
             
             conn.query('SELECT * FROM users WHERE id = ?;', data_id, function(err2, rows2, fields) {
