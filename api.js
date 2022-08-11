a75e9ebe5bbe69a52ccb4a61406cbe6c09d1d3d0
espatium <99029937+espatium@users.noreply.github.com>
2022-08-11 22:51:50 +0900

    Update api.js

--- api.js
+++ api.js
@@ -192,7 +192,7 @@ app.post('/api/ox_1_1_user_correct_month1/add', function(req, res) {
     var sql = 'INSERT INTO s_db_ox_1_1_user_correct_month1 (user_num, 1, 0,0,0,0,0,0,0,0,0,0,\
                0,0,0,0,0,0,0,0,0,0,  0,0,0,0,0,0,0,0,0,0,  0,0,0,0,0,0,0,0,0,0,  0,0,0,0,0,0,0,0,0,0,\
                0,0,0,0,0,0,0,0,0,0,  0,0,0,0,0,0,0,0,0,0,  0,0,0,0,0,0,0,0,0,0,  0,0,0,0,0,0,0) VALUES (?)';
-    conn.query(sql, [nickname, email, join_date, last_login_date, user_version, level], (err, rows, fields) => {
+    conn.query(sql, [user_num], (err, rows, fields) => {
         if(err) {
             console.log(err);
             res.status(500).send('Internal Server Error');
